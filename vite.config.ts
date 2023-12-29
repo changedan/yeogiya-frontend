09e8cae6423e5f9449c862bcccc8dc320acffb08
parkgadan <>
2023-12-29 14:31:49 +0000

    Update from https://github.com/parkgadan/yeogiya-frontend/commit/363d555301bd986cd512165c20c43866efc7271c

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
@@ -15,11 +15,4 @@ export default defineConfig({
     ,
     tsconfigPaths(),
   ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
-    },
-  },
 });
